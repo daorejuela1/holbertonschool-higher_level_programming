4c97210e10191c6df37650b1e15948f342bba253
daorejuela1 <vids@hotmail.es>
2020-04-27 19:12:27 +0000

    remove trailing whitespace

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {0:s}!".format(str1))
+strcat = str1 + " " + str2
+print("Welcome to {}!".format(strcat))
