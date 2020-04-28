81aec0b924fa9a05fe51aa5a4cd565bc7eddb802
daorejuela1 <vids@hotmail.es>
2020-04-28 17:01:20 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for units in range(0, 9):
+    for decs in range(units + 1, 10):
+        if (units != 8 or decs != 9):
+            print(units, end="")
+            print(decs, end=", ")
+print(units, end="")
+print(decs)
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        if (ord(str[i]) < 97 or ord(str[i]) > 122):
+            upper_character = str[i]
+        else:
+            upper_character = chr(ord(str[i]) - 32)
+        if (i != len(str) - 1):
+            print(upper_character, end="")
+        else:
+            print(upper_character)
