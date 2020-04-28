383515767b2f97b4c6f1511e51c80061134bc13e
daorejuela1 <vids@hotmail.es>
2020-04-28 18:32:21 +0000

    testing using format in printf

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        print(chr(ord(str[i])), end="")
-        if (i == len(str) - 1):
-            print("")
+        if (ord(str[i]) < 97 or ord(str[i]) > 122):
+            upper_character = str[i]
+        else:
+            upper_character = chr(ord(str[i]) - 32)
+        if (i != len(str) - 1):
+            print("{}".format(upper_character), end="")
+        else:
+            print("{}".format(upper_character))
