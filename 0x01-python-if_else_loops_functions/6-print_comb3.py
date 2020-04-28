338afd2787cdd1742f8a2bcf7e6b79251703bb81
daorejuela1 <vids@hotmail.es>
2020-04-28 17:16:40 +0000

    fixed requirement max 3 print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,6 @@
 for units in range(0, 9):
     for decs in range(units + 1, 10):
         if (units != 8 or decs != 9):
-            print(units, end="")
-            print(decs, end=", ")
+            print("{:d}{:d}".format(units, decs), end=", ")
 print(units, end="")
 print(decs)
