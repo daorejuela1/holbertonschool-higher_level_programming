44e5080636aa327a577bef8acf03f646016eb71f
daorejuela1 <vids@hotmail.es>
2020-04-28 18:28:24 +0000

    testing if no variables are allowed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        if (ord(str[i]) < 97 or ord(str[i]) > 122):
-            change = 0
-        else:
-            change = 32
-        print(chr(ord(str[i]) - change), end="")
+        print(chr(ord(str[i])), end="")
         if (i == len(str) - 1):
             print("")
