f4e57aa8c026300d80359443512b03c740489142
daorejuela1 <vids@hotmail.es>
2020-04-28 17:58:32 +0000

    fixed case 0 for task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     copy = [None] * len(str)
     result = ""
-    if (n <= len(str) and n > 0):
+    if (n <= len(str) and n >= 0):
         copy[0:n] = str[0:n]
         copy[n + 1:len(str)] = str[n + 1:len(str)]
         copy = list(filter(None, copy))
