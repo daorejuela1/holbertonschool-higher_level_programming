aeb416a0d2f00272ac11f46a8e7ee0df6b42936a
daorejuela1 <vids@hotmail.es>
2020-04-30 19:24:12 +0000

    added first advance task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv) != 4):
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit(1)
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    symbol = sys.argv[2]
+    operators = {
+        '+': add(a, b),
+        '-': sub(a, b),
+        '*': mul(a, b),
+        '/': div(a, b)
+    }
+    try:
+        print("{:d} {} {:d} = {:d}".format(a, symbol, b, operators[symbol]))
+    except KeyError:
+        print("Unknown operator. Available operators: +, -, * and /")
