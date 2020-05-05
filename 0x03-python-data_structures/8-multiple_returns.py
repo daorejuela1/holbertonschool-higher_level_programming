431dfb2f46b5b691dff5222d084af7a99619cf0e
daorejuela1 <vids@hotmail.es>
2020-05-05 19:54:48 +0000

    added task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    """returns the lenght of a string and first character"""
+    if sentence:
+        string_length = len(sentence)
+        first_character = sentence[0]
+    else:
+        string_length = 0
+        first_character = None
+    return (string_length, first_character)
