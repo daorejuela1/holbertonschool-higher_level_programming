f89167def40d5eb10cc99f938616e5c76c19e7e3
daorejuela1 <vids@hotmail.es>
2020-05-05 19:22:07 +0000

    added task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	"""Prints a matrix in python"""
-	for col in range(0, 3):
-		for row in range(0, 3):
-			print("{:d}".format(matrix[col][row]), end=", ")
-			if (row == 2):
-				print()
+    """Prints a matrix in python"""
+    num_lists = len(matrix)
+    len_list = len(matrix[0])
+    for col in range(0, num_lists):
+        for row in range(0, len_list):
+            print("{:d}".format(matrix[col][row]), end="")
+            if (row == len_list - 1):
+                print()
+            else:
+                print("", end=", ")
