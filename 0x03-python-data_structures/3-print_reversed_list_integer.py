9c7a2b4fd314eb9fb2c412fd58cb814f58f8f3a5
daorejuela1 <vids@hotmail.es>
2020-05-05 18:59:26 +0000

    used reverse instead of -1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints list in reverse"""
-    for i in my_list[::-1]:
-        print("{:d}".format(i))
+    for i in reversed(range(0, len(my_list))):
+        print("{:d}".format(my_list[i]))
