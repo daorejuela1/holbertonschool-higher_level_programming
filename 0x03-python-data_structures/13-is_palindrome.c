053db453fde4d08825c103999f3275ca632fa0eb
daorejuela1 <vids@hotmail.es>
2020-05-05 22:13:07 +0000

    added task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ *is_palindrome - function to detect if a single list is a palindrome
+ *@head: head of the single linked list
+ *Return: 0 if is not, 1 if is it
+ */
+int is_palindrome(listint_t **head)
+{
+	void *origin;
+	int len = 0, *tmp_array, i = 0;
+
+	if (*head == NULL)
+		return (1);
+	origin = *head;
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+		len++;
+	}
+	len--;
+	*head = origin;
+	tmp_array = malloc(((len + 1) / 2) * sizeof(int));
+	for (i = 0; i <= len; i++)
+	{
+		if (i <= (len / 2))
+		{
+			tmp_array[i] = (*head)->n;
+			*head = (*head)->next;
+		}
+		else
+		{
+			if (tmp_array[len - i] != (*head)->n)
+			{
+				free(tmp_array);
+				*head = origin;
+				return (0);
+			}
+			*head = (*head)->next;
+		}
+	}
+	free(tmp_array);
+	*head = origin;
+	printf("Valor de: %d\n", len);
+	return (1);
+}
