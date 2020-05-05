963379413fb3f538abe6113ea7d5b6ad7c1a9ae4
daorejuela1 <vids@hotmail.es>
2020-05-05 20:18:40 +0000

    uploaded task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    logic_list = []
+    for num in my_list:
+        if (num % 2 == 0):
+            logic_list.append(True)
+        else:
+            logic_list.append(False)
+    return (logic_list)
