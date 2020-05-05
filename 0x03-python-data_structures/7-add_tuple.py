d28aceaec74911775cdfcc01e9d0021f577137c6
daorejuela1 <vids@hotmail.es>
2020-05-05 19:44:24 +0000

    fixed pep8 in task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i in range (0, 2):
+    for i in range(0, 2):
         try:
             tuple_b[i]
         except:
             tuple_b = tuple_b + (0,)
-    for i in range (0, 2):
+    for i in range(0, 2):
         try:
             tuple_a[i]
         except:
