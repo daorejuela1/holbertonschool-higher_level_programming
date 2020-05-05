d894061740916cce8bbac89b739880c0e550f5e3
daorejuela1 <vids@hotmail.es>
2020-05-05 19:24:55 +0000

    added condition when matrix is null

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -2,9 +2,9 @@
 print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
 
 matrix = [
-    [1, 2, 3, 5],
-    [4, 5, 6, 7],
-    [7, 8, 9, 2]
+    [1, 2],
+    [4, 5],
+    [7, 8]
 ]
 
 print_matrix_integer(matrix)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix in python"""
-    num_lists = len(matrix)
-    len_list = len(matrix[0])
-    for col in range(0, num_lists):
-        for row in range(0, len_list):
-            print("{:d}".format(matrix[col][row]), end="")
-            if (row == len_list - 1):
-                print()
-            else:
-                print("", end=", ")
+    if matrix[0]:
+        num_lists = len(matrix)
+        len_list = len(matrix[0])
+        for col in range(0, num_lists):
+            for row in range(0, len_list):
+                print("{:d}".format(matrix[col][row]), end="")
+                if (row == len_list - 1):
+                    print()
+                else:
+                    print("", end=", ")
+    else:
+        print()
