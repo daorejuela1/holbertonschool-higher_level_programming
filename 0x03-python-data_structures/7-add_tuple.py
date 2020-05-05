c8b1c27da9f65cd6a7164cb618e4c55a6e4ea9a5
daorejuela1 <vids@hotmail.es>
2020-05-05 19:43:08 +0000

    added task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    for i in range (0, 2):
+        try:
+            tuple_b[i]
+        except:
+            tuple_b = tuple_b + (0,)
+    for i in range (0, 2):
+        try:
+            tuple_a[i]
+        except:
+            tuple_a = tuple_a + (0,)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (result)
