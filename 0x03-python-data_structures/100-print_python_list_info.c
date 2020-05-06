b9ec26f85235710a3768cd21883c7a8c607f9e8e
daorejuela1 <vids@hotmail.es>
2020-05-06 01:18:04 +0000

    added last advance

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+/**
+ * print_python_list_info - this prints the data type inside lists
+ * @p: - Python data type that's a list
+ * Returns: Nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+	int i = 0;
+	PyListObject *ptr;
+
+	ptr = (PyListObject *)p;
+	printf("[*] Size of the Python List = %d\n", (int)(PyList_Size(p)));
+	printf("[*] Allocated = %d\n", (int)ptr->allocated);
+	for (i = 0; i < (int)(PyList_Size(p)); i++)
+		printf("Element [%d] %s\n", i, Py_TYPE(ptr->ob_item[i])->tp_name);
+}
