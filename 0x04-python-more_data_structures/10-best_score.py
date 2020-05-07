26645debe145593384e4348ca8685680a9740858
daorejuela1 <vids@hotmail.es>
2020-05-07 19:20:24 +0000

    fixed logic of task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,12 +4,7 @@ def best_score(a_dictionary):
         return
     max_value = list(a_dictionary.values())[0]
     for key in a_dictionary:
-        max_value = greater_than(a_dictionary[key], max_value)
-    return (max_value)
-
-
-def greater_than(a, b):
-    if (a > b):
-        return (a)
-    else:
-        return (b)
+        if (a_dictionary[key] > max_value):
+            max_value = a_dictionary[key]
+            final_key = key
+    return (final_key)
