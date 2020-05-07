431933948fe802b2d241ed7a9dcd3f5cbce1cd3d
daorejuela1 <vids@hotmail.es>
2020-05-07 19:33:30 +0000

    uploaded first advance

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
+def weight_average(my_list=[]):
+    numerator = 0
+    denominator = 0
+    if not my_list:
+        return (0)
+    for i in range(0, len(my_list)):
+        numerator = my_list[i][0] * my_list[i][1] + numerator
+    for i in range(0, len(my_list)):
+        denominator = my_list[i][1] + denominator
+    return(numerator/denominator)
