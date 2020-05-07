8a4dce5ed8cdaec01c44d6709c4ebb06babcdbf6
daorejuela1 <vids@hotmail.es>
2020-05-07 20:43:16 +0000

    changed for loop for other map fucntion

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(i ** 2 for i in x[0:len(x)]), matrix)))
+    return(list(map(lambda x: list(map(lambda n: n**2, x[0:len(x)])), matrix)))
