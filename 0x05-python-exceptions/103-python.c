70f292247ee4d731033b59e91081861de506d86a
daorejuela1 <vids@hotmail.es>
2020-05-18 20:22:00 +0000

    added logic for last advance

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,112 @@
+#include <Python.h>
+#include <stdio.h>
+#define PY_REP(x) (((PyObject *)(x))->ob_type)
+#define DATATYPE PY_REP(((PyListObject *)(p))->ob_item[i])->tp_name
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+/**
+ *print_python_list - function to print about list
+ *@p: pointer to refer to a list of python
+ *Return: Nothing
+ */
+void print_python_list(PyObject *p)
+{
+	int i = 0, tamano = 0;
+
+	printf("[*] Python list info\n");
+	if (PyList_Check(p))
+	{
+		tamano = (int)(((PyVarObject *)(p))->ob_size);
+
+		printf("[*] Size of the Python List = %d\n", tamano);
+		printf("[*] Allocated = %d\n", (int)(((PyListObject *)(p))->allocated));
+		for (i = 0; i < tamano; i++)
+		{
+			printf("Element %d: %s\n", i, DATATYPE);
+			if (PyBytes_Check((((PyListObject *)(p))->ob_item[i])))
+				print_python_bytes((((PyListObject *)(p))->ob_item[i]));
+			else if (PyFloat_Check((((PyListObject *)(p))->ob_item[i])))
+				print_python_float((((PyListObject *)(p))->ob_item[i]));
+
+		}
+	}
+	else
+	{
+		printf("  [ERROR] Invalid List Object\n");
+	}
+}
+/**
+ *print_python_bytes - function to print about list
+ *@p: pointer to refer to a byte object of python
+ *Return: Nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+	int tamano = 0, printed = 0, i = 0;
+	char *buffer = NULL;
+	Py_ssize_t length = NULL;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p))
+	{
+		PyBytes_AsStringAndSize(p, &buffer, &length);
+		tamano = (int)length;
+		if (tamano >= 10)
+			printed = 10;
+		else
+			printed = tamano + 1;
+		printf("  size: %d\n", tamano);
+		printf("  trying string: %s\n", buffer);
+		printf("  first %d bytes: ", printed);
+		for (i = 0; i < printed; i++)
+		{
+			printf("%02x", (unsigned char)*(buffer + i));
+			if (i < printed - 1)
+				printf(" ");
+		}
+			printf("\n");
+	}
+	else
+		printf("  [ERROR] Invalid Bytes Object\n");
+}
+/**
+ *trim_zeros - this will cut a string with the zero delimiter
+ *@x: string to cut for float logic
+ *Return: Nothing
+ */
+void trim_zeros(char *x)
+{
+	int i, length, flag_passed = 0;
+
+	length = strlen(x) - 1;
+	for (i = 0; i < length; i++)
+	{
+		if (x[i] == '.' && x[i + 1] == '0')
+			x[i + 2] = 0;
+		else if (x[i] == '.')
+			flag_passed = 1;
+		if (flag_passed == 1 && x[i] == '0')
+			x[i] = 0;
+	}
+}
+/**
+ *print_python_float - function to print about float objects
+ *@p: pointer to refer to a list of python
+ *Return: Nothing
+ */
+void print_python_float(PyObject *p)
+{
+	char float_number[15];
+
+	printf("[.] float object info\n");
+	if (PyFloat_Check(p))
+	{
+		sprintf(float_number, "%0.15f", ((PyFloatObject *)(p))->ob_fval);
+		trim_zeros(float_number);
+		printf("  value: %s\n", float_number);
+	}
+	else
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+	}
+}
