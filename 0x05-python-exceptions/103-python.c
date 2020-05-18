b31394eab67825620dc86d7f1201e4972da65579
daorejuela1 <vids@hotmail.es>
2020-05-18 22:38:26 +0000

    testing other double to string

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -5,7 +5,6 @@
 #define MAX_SIZE 15
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
-void trim_zeros(char *x);
 void print_python_float(PyObject *p);
 
 /**
@@ -88,35 +87,7 @@ void print_python_bytes(PyObject *p)
 		fflush(stdout);
 	}
 }
-/**
- *trim_zeros - this will cut a string with the zero delimiter
- *@x: string to cut for float logic
- *Return: Nothing
- */
-void trim_zeros(char *x)
-{
-	int i, length, flag_passed = 0;
 
-	length = strlen(x) - 1;
-	for (i = 0; i < length; i++)
-	{
-		if (x[i] == '.' && x[i + 1] == '0')
-		{
-			i = i + 2;
-			x[i] = 0;
-			break;
-		}
-		else if (x[i] == '.')
-			flag_passed = 1;
-		if (flag_passed == 1 && x[i] == '0')
-		{
-			x[i] = 0;
-			break;
-		}
-	}
-	for (; i < length; i++)
-		x[i + 1] = 0;
-}
 /**
  *print_python_float - function to print about float objects
  *@p: pointer to refer to a list of python
@@ -124,15 +95,16 @@ void trim_zeros(char *x)
  */
 void print_python_float(PyObject *p)
 {
-	char float_number[MAX_SIZE];
+	float value;
+	char *string;
 
 	printf("[.] float object info\n");
 	fflush(stdout);
 	if (PyFloat_Check(p))
 	{
-		sprintf(float_number, "%0.15f", ((PyFloatObject *)(p))->ob_fval);
-		trim_zeros(float_number);
-		printf("  value: %s\n", float_number);
+		value = ((PyFloatObject *)(p))->ob_fval;
+		string = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+		printf("  value: %s\n", string);
 		fflush(stdout);
 	}
 	else
