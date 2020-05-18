a2740f8b6189afd625bfef901611a599e11c1bb3
daorejuela1 <vids@hotmail.es>
2020-05-18 20:24:47 +0000

    added fflush

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -14,15 +14,19 @@ void print_python_list(PyObject *p)
 	int i = 0, tamano = 0;
 
 	printf("[*] Python list info\n");
+	fflush(stdout);
 	if (PyList_Check(p))
 	{
 		tamano = (int)(((PyVarObject *)(p))->ob_size);
 
 		printf("[*] Size of the Python List = %d\n", tamano);
+		fflush(stdout);
 		printf("[*] Allocated = %d\n", (int)(((PyListObject *)(p))->allocated));
+		fflush(stdout);
 		for (i = 0; i < tamano; i++)
 		{
 			printf("Element %d: %s\n", i, DATATYPE);
+			fflush(stdout);
 			if (PyBytes_Check((((PyListObject *)(p))->ob_item[i])))
 				print_python_bytes((((PyListObject *)(p))->ob_item[i]));
 			else if (PyFloat_Check((((PyListObject *)(p))->ob_item[i])))
@@ -33,6 +37,7 @@ void print_python_list(PyObject *p)
 	else
 	{
 		printf("  [ERROR] Invalid List Object\n");
+		fflush(stdout);
 	}
 }
 /**
@@ -47,6 +52,7 @@ void print_python_bytes(PyObject *p)
 	Py_ssize_t length = NULL;
 
 	printf("[.] bytes object info\n");
+	fflush(stdout);
 	if (PyBytes_Check(p))
 	{
 		PyBytes_AsStringAndSize(p, &buffer, &length);
@@ -56,18 +62,25 @@ void print_python_bytes(PyObject *p)
 		else
 			printed = tamano + 1;
 		printf("  size: %d\n", tamano);
+		fflush(stdout);
 		printf("  trying string: %s\n", buffer);
+		fflush(stdout);
 		printf("  first %d bytes: ", printed);
+		fflush(stdout);
 		for (i = 0; i < printed; i++)
 		{
 			printf("%02x", (unsigned char)*(buffer + i));
+			fflush(stdout);
 			if (i < printed - 1)
 				printf(" ");
+				fflush(stdout);
 		}
 			printf("\n");
+			fflush(stdout);
 	}
 	else
 		printf("  [ERROR] Invalid Bytes Object\n");
+		fflush(stdout);
 }
 /**
  *trim_zeros - this will cut a string with the zero delimiter
@@ -99,14 +112,17 @@ void print_python_float(PyObject *p)
 	char float_number[15];
 
 	printf("[.] float object info\n");
+	fflush(stdout);
 	if (PyFloat_Check(p))
 	{
 		sprintf(float_number, "%0.15f", ((PyFloatObject *)(p))->ob_fval);
 		trim_zeros(float_number);
 		printf("  value: %s\n", float_number);
+		fflush(stdout);
 	}
 	else
 	{
 		printf("  [ERROR] Invalid Float Object\n");
+		fflush(stdout);
 	}
 }
