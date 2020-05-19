c7ae8778467dd8992d7c611f9732c4a7f1b526d2
daorejuela1 <vids@hotmail.es>
2020-05-19 20:28:04 +0000

    uploaded advance tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square():
+    """This class defines a Square """
+    def __init__(self, size):
+        """Initialize square size"""
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square():
+    """This class defines a Square """
+    def __init__(self, size=0):
+        """Initialize square size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the square area -> a * b"""
+        return self.__size * self.__size
