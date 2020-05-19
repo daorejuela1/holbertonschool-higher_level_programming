d0a07d7526ad74aa194e7a83b6dca2bfbb7fc68b
daorejuela1 <vids@hotmail.es>
2020-05-19 23:20:04 +0000

    replaced _MagicClass__radius by private element radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,15 +7,15 @@ class MagicClass():
     """class to represent a circle"""
     def __init__(self, radius):
         """get radius type"""
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """area = r^2 * pi"""
-        return((self._MagicClass__radius ** 2) * math.pi)
+        return((self.__radius ** 2) * math.pi)
 
     def circumference(self):
         """circumference = 2 * r * pi"""
-        return(2 * math.pi * self._MagicClass__radius)
+        return(2 * math.pi * self.__radius)
