ef54bcd9eae171868a03506437c3c5d2a4d97871
daorejuela1 <vids@hotmail.es>
2020-06-04 01:03:36 +0000

    uploaded first advance

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" append after finding """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function appends text after finding text in a sequence"""
+    with open(filename, 'r') as my_file:
+        list_values = my_file.readlines()
+
+    for i in range(len(list_values)):
+        value = list_values[i].find(search_string)
+        if (value != -1):
+            list_values.insert(i + 1, new_string)
+
+    with open(filename, 'w') as my_file:
+        my_file.write("".join(list_values))
