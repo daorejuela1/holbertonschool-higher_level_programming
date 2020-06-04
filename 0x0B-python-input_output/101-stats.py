e605818b2f5374b3f9dd8496d856135b6be69268
daorejuela1 <vids@hotmail.es>
2020-06-04 04:34:06 +0000

    removed main function

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,28 +5,14 @@ import sys
 
 def print_pretty(size, code_dict):
     """parse important data"""
-    print("File size: " + str(size[0]))
+    print("File size: " + str(size))
     for key, value in sorted(code_dict.items()):
         if (value != 0):
             print("{}: {}".format(key, value))
 
-
-def main(size, code_dict):
-    """main logic to read stdin and obtain data"""
-    line_counter = 0
-    for line in sys.stdin:
-        line_counter += 1
-        code = line.split()[7]
-        size[0] += int(line.split()[8])
-        if code in code_dict:
-            code_dict[code] += 1
-
-        if (line_counter % 10 == 0):
-            print_pretty(size, code_dict)
-
 if __name__ == '__main__':
     """init code to print the parsed data"""
-    size = [0]
+    size = 0
     code_dict = {
         "200": 0,
         "301": 0,
@@ -38,6 +24,15 @@ if __name__ == '__main__':
         "500": 0
     }
     try:
-        main(size, code_dict)
+        line_counter = 0
+        for line in sys.stdin:
+            line_counter += 1
+            code = line.split()[7]
+            size += int(line.split()[8])
+            if code in code_dict:
+                code_dict[code] += 1
+            if (line_counter % 10 == 0):
+                print_pretty(size, code_dict)
     except KeyboardInterrupt:
         print_pretty(size, code_dict)
+        raise
