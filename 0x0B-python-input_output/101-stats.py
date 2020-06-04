150c021f0eb89b7180e69904888c8fc28fcfa581
daorejuela1 <vids@hotmail.es>
2020-06-04 15:01:25 +0000

    added secure code

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -26,10 +26,9 @@ if __name__ == '__main__':
     try:
         line_counter = 0
         for line in sys.stdin:
-            if (len(line.split()) != 9):
-                line_counter += 1
-                continue
             line_counter += 1
+            if (len(line.split()) < 9):
+                continue
             code = line.split()[7]
             size += int(line.split()[8])
             if code in code_dict:
