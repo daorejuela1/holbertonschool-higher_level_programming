fd4f991f16b699a3e6c48697ce2fd38cb5720bc7
daorejuela1 <vids@hotmail.es>
2020-06-04 01:23:06 +0000

    fixed adding when len is not the same

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,16 @@ def append_after(filename="", search_string="", new_string=""):
         return
     with open(filename, 'r') as my_file:
         list_values = my_file.readlines()
-
-    for i in range(len(list_values)):
+    if not list_values:
+        return
+    i = 0
+    variable_len = len(list_values)
+    while i != variable_len:
         value = list_values[i].find(search_string)
         if (value != -1):
             list_values.insert(i + 1, new_string)
+        i += 1
+        variable_len = len(list_values)
 
     with open(filename, 'w') as my_file:
         my_file.write("".join(list_values))
