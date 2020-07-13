9100899555cc0609bfc834659607d15548bf56b4
daorejuela1 <vids@hotmail.es>
2020-07-13 22:39:32 +0000

    added order desc by score in task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- list tables of databases
 -- calculates grouped scores
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
