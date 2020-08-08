7c22129314cdc0f796601250c5b86862e63861c6
daorejuela1 <vids@hotmail.es>
2020-08-08 19:44:06 +0000

    added ordered by to output desire order

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -25,9 +25,9 @@ def main():
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state, city in session.query(State,
-                                     City).filter(City.state_id == State.id):
-        print("{}: ({}) {}".format(state.name, state.id, city.name))
+    for state, city in (session.query(State, City).order_by(City.id).
+                        filter(City.state_id == State.id)):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
 
 if __name__ == '__main__':
