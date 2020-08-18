393bdfa210cb125d345a8cfe8529b65379d78dcf
daorejuela1 <vids@hotmail.es>
2020-08-18 18:04:21 +0000

    added try of task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Finds peaks using python
+"""
+def find_peak(list_of_integers):
+    """
+    This function returns the peak of a list
+    """
+    if list_of_integers:
+        return(max(list_of_integers))
+    return(None)
