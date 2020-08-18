9f9439b449192117f61a3668c65787aae2545b80
daorejuela1 <vids@hotmail.es>
2020-08-18 18:19:22 +0000

    checking time complexity of sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,5 +9,5 @@ def find_peak(list_of_integers):
     This function returns the peak of a list
     """
     if list_of_integers:
-        return(max(list_of_integers))
+        return(sorted(list_of_integers)[-1])
     return(None)
