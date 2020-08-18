127d0aa28eaef7d683b6000bac28c36b27bf1d6f
daorejuela1 <vids@hotmail.es>
2020-08-18 17:17:50 +0000

    changed logic with -f flag

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#prints content if 200 is in valu
-#prints content if 200 is in valuee
-condition=$(curl -sLI "$1" | grep "200 OK" | cut -d " " -f2)
-if [[ $condition == 200 ]]
-then
-	curl -sL "$1"
-fi
+#prints content if 200 is in value
+curl -sfL "$1"
