5f8d27beaa8bc8c6219eb961273370811cdce5d1
daorejuela1 <vids@hotmail.es>
2020-08-24 19:46:51 +0000

    used inputs to get user and token

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,8 +8,11 @@ import sys
 if __name__ == "__main__":
 
     url = "https://api.github.com/user"
-    user = "daorejuela1"
-    one_way_token = "aba627147b5c2e4b74aa29d68be2aea818e299aa"
+    user = sys.argv[1]
+    one_way_token = sys.argv[2]
     response = requests.get(url, auth=(user, one_way_token))
     my_data = response.json()
-    print(my_data.get('id'))
+    if not my_data:
+        print(my_data)
+    else:
+        print(my_data.get('id'))
