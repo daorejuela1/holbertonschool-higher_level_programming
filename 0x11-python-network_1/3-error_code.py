f65f36ac4324fbbc0a509dedb37cae2b16b040e4
daorejuela1 <vids@hotmail.es>
2020-08-24 18:19:22 +0000

    added error handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+This file creates a request on the internet
+"""
+from urllib import request
+from urllib import parse
+from urllib import error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with request.urlopen(url) as response:
+            my_data = response.read()
+            print(my_data.decode())
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code))
