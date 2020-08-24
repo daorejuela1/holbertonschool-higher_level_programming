12054a80d956c93363eb615476d2b71fb707e827
daorejuela1 <vids@hotmail.es>
2020-08-24 19:39:54 +0000

    uploaded task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This file gets info from an url
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "https://api.github.com/user"
+    user = "daorejuela1"
+    one_way_token = "aba627147b5c2e4b74aa29d68be2aea818e299aa"
+    response = requests.get(url, auth=(user, one_way_token))
+    my_data = response.json()
+    print(my_data.get('id'))
