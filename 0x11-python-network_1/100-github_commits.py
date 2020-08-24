b83638141bdfc5967f22a2cc1de4622e46f2b403
daorejuela1 <vids@hotmail.es>
2020-08-24 20:05:53 +0000

    uploaded first advance task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This file gets info from an url
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    repository = sys.argv[1]
+    user = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(repository, user)
+    response = requests.get(url)
+    my_data = response.json()
+    for i in range(10):
+        print("{} {}".format(my_data[i].get('sha'),
+                             my_data[i].get('commit').
+                             get('author').get('name')))
