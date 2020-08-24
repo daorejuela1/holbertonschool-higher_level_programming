77c03183176810f58fcdf714e03c092bcf74504d
daorejuela1 <vids@hotmail.es>
2020-08-24 16:57:51 +0000

    used with statement for task 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This file creates a request on the internet
+"""
+from urllib import request
+import sys
+
+url = "https://intranet.hbtn.io/status"
+my_data_request = request.urlopen(url)
+key_values = ['type', 'content', 'utf8 content']
+values = []
+my_data = my_data_request.read()
+values.append(type(my_data))
+values.append(my_data)
+values.append(my_data.decode())
+print("Body response:")
+for i in range(3):
+    print("\t- {}: {}".format(key_values[i], values[i]))
