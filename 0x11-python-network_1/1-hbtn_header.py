182a4e00cefd2100a84320e02fde428d009816ef
daorejuela1 <vids@hotmail.es>
2020-08-24 17:02:42 +0000

    added code to not execute when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,14 +5,16 @@ This file creates a request on the internet
 from urllib import request
 import sys
 
-url = "https://intranet.hbtn.io/status"
-my_data_request = request.urlopen(url)
-key_values = ['type', 'content', 'utf8 content']
-values = []
-my_data = my_data_request.read()
-values.append(type(my_data))
-values.append(my_data)
-values.append(my_data.decode())
-print("Body response:")
-for i in range(3):
-    print("\t- {}: {}".format(key_values[i], values[i]))
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    my_data_request = request.urlopen(url)
+    key_values = ['type', 'content', 'utf8 content']
+    values = []
+    my_data = my_data_request.read()
+    values.append(type(my_data))
+    values.append(my_data)
+    values.append(my_data.decode())
+    print("Body response:")
+    for i in range(3):
+        print("\t- {}: {}".format(key_values[i], values[i]))
