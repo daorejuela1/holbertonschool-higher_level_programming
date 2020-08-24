b73cb0d2270f4b7cfe1ef4bbfa9cc3f5e2daf98b
daorejuela1 <vids@hotmail.es>
2020-08-24 18:32:39 +0000

    added task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This file creates a request on the internet
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    key = ['type', 'content']
+    response = requests.get(url)
+    print("Body response:")
+    value = [type(response.text), response.text]
+    for i in range(2):
+        print("\t- {}: {}".format(key[i], value[i]))
