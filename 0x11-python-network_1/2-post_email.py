7e0338fdd0e148824125e7d4ca7f3dd529497ba7
daorejuela1 <vids@hotmail.es>
2020-08-24 18:04:49 +0000

    uploaded task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This file creates a request on the internet
+"""
+from urllib import request
+from urllib import parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    address = sys.argv[2]
+    data = parse.urlencode({'email': address})
+    with request.urlopen(url, data) as response:
+        my_data = response.read()
+        print(my_data.decode())
