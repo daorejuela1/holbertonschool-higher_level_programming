0b4cc029eac73f9cefa37d0ff886645d3545bb47
daorejuela1 <vids@hotmail.es>
2020-08-24 18:46:07 +0000

    uploaded task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+This file gets info from an url
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if (response.status_code >= 400):
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
