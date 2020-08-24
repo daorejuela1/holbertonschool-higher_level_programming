e4d7b16519fb9cfd5c8fe165b4a904105d01c960
daorejuela1 <vids@hotmail.es>
2020-08-24 19:04:11 +0000

    added task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+This file gets info from an url
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    url = "http://0.0.0.0:5000/search_user"
+    if (len(sys.argv) > 1):
+        value = sys.argv[1]
+    else:
+        value = ""
+    dataset = {'q': value}
+    response = requests.post(url, data=dataset)
+    try:
+        json = response.json()
+        if not json:
+            print("No result")
+        else:
+            value = json.loads
+            print("[{}] {}".format(value['id'], value['name']))
+    except ValueError:
+        print("Not a valid JSON")
