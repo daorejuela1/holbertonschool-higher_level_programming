26bca4eda9eb99898d072390e5166539aae3f350
daorejuela1 <vids@hotmail.es>
2020-08-24 18:35:57 +0000

    added task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+This file obtains a header by using request
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
