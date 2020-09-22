18a42b85428c1edad5d08e2ad9bb05761fb898bb
daorejuela1 <daorejuela1@outlook.com>
2020-09-22 19:22:23 +0000

    removed else condition when printing error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const MyFiles = process.argv.slice(2);
 fs.readFile(MyFiles[0], 'utf8', function (err, data) {
   if (err) console.log('error', err);
-  else console.log(data);
+  console.log(data);
 });
