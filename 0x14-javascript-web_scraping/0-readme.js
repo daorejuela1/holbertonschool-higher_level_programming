9f9721eaad62019ffcfd1bd23a35f854ac877d6d
daorejuela1 <daorejuela1@outlook.com>
2020-09-22 19:27:28 +0000

    removed key Error when error was founded

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 const fs = require('fs');
 const MyFiles = process.argv.slice(2);
 fs.readFile(MyFiles[0], 'utf8', function (err, data) {
-  if (err) console.log('error', err);
+  if (err) console.log(err);
   else process.stdout.write(data);
 });
