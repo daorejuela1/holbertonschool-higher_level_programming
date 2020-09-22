717040b325d5a96ac524523f1a76ab6099e8486f
daorejuela1 <daorejuela1@outlook.com>
2020-09-22 22:58:38 +0000

    added async wait to wait for values before printing them

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,31 @@
 const request = require('request');
 const MyFiles = process.argv.slice(2);
 let data;
-request('https://swapi-api.hbtn.io/api/films/' + MyFiles[0],
-  function (error, response, body) {
-    if (error) console.error('error:', error);
-    else {
-      data = JSON.parse(body);
-      for (const i in data.characters) {
-        let charname;
-        request(data.characters[i], function (error, response, body) {
-          if (error) console.error('error:', error);
-          else {
-            charname = JSON.parse(body);
-            console.log(charname.name);
-          }
-        });
+let i;
+let MyList = [];
+function GetCharId (id) {
+  request('https://swapi-api.hbtn.io/api/films/' + id,
+    async function (error, response, body) {
+      if (error) console.error(error);
+      else {
+        data = JSON.parse(body);
+        MyList = data.characters;
+        for (i = 0; i < MyList.length; i++) {
+          const name = await GetName(MyList[i]);
+          console.log(name);
+        }
       }
-    }
+    });
+}
+
+function GetName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      else {
+        resolve(JSON.parse(body).name);
+      }
+    });
   });
+}
+GetCharId(MyFiles[0]);
