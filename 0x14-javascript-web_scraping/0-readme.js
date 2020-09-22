e8db26ab15537bf2696b9c92f915d80169c67db0
daorejuela1 <daorejuela1@outlook.com>
2020-09-22 19:25:11 +0000

    used process write to avoid new line printing

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const MyFiles = process.argv.slice(2);
 fs.readFile(MyFiles[0], 'utf8', function (err, data) {
   if (err) console.log('error', err);
-  console.log(data);
+  else process.stdout.write(data);
 });
